f4b48d70a97fd551d7ac419b5e9bf764cd5f808b
Francary <francarymolinapaez@gmail.com>
2024-12-21 17:01:28 -0300

    API PEXELS - Consulta una lista de Productos y Crea las Card

--- javascript/api.js
+++ javascript/api.js
@@ -12,24 +12,36 @@ const buscarProductos = async () => {
         // URL
         // const URL = `https://api.pexels.com/v1/search/?page=${page}&per_page=${per_page}&query=${query}`
       
-
         // Buscar foto por el ID
+        const arryFotos = [1640777,70497,262959,769969,361184,1126359,376464,2418486]
 
-        const arryFotos = [70497, 262959 ,769969]
-        const URL = `https://api.pexels.com/v1/photos/${arryFotos[2]}`
-     
+        for (const id of arryFotos) {
+        const URL = `https://api.pexels.com/v1/photos/${id}`
         const res = await fetch(URL, {
             method: "GET",
             headers: {
                 "Authorization": APIKEY
             }
         })
-
-        const data = await res.json();
+        
+        const data = await res.json();      
         console.log("Respuesta de Consulta:", data);  
 
-        
-        
+        // Funcion de para calcular un precio segun el id del Producto
+       
+        let precio =  Math.round( data.id /  ( 1 + '0'.repeat( Number( data.id.toString().length - 2) ) ))
+
+        let cardProducto =
+        `        
+            <div class="card-productos">
+                <img class="producto" src="${data.src.tiny}" alt="${data.alt}">
+                <p> U$D ${precio}</p>
+                <button class="btn btn-primary">Pedir</button>
+            </div>
+        `
+
+        document.getElementById("container-productos").insertAdjacentHTML("beforeend", cardProducto)  
+    }
     } catch (error) {
         
     }

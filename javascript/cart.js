71f6669207979d04887fc4ead09bb903d60aa04a
Francary <francarymolinapaez@gmail.com>
2024-12-22 13:15:20 -0300

    cart 3.0 Boton Finalizar Compra y Boton Eliminar Compra

--- javascript/cart.js
+++ javascript/cart.js
@@ -16,12 +16,47 @@ const deleteItem = (index) => {
         ` <h3>Eliminastes todos los platos del Pedido</h3>
         
         `
-
     }else{
 
         sumaCompra()
     }
 };
+
+const finalizarPedido =() =>{
+    localStorage.removeItem("Pedido")
+    alert("Pago Procesado Correctamente")
+    buscarProductos()
+    if(cart.length === 0 ){document.getElementById("total-compra").innerHTML = 
+        ` 
+            <h3>Pago procesado Correctamente !</h3>
+            <a href="/index.html">
+                <button class="btn btn-primary"> Volver a Comprar</button>
+            <a>
+        
+        `
+    }else{
+
+        sumaCompra()
+    }
+}
+
+const eliminarPedido =() =>{
+    localStorage.removeItem("Pedido")
+    alert("Pedido Eliminado")
+    buscarProductos()
+    if(cart.length === 0 ){document.getElementById("total-compra").innerHTML = 
+        ` 
+            <h3>Pedido Eliminado Correctamente!</h3>
+            <a href="/index.html">
+                <button class="btn btn-primary"> Ir A Comprar</button>
+            <a>
+        
+        `
+    }else{
+
+        sumaCompra()
+    }
+}
 
 const sumaCompra = () =>{
     try {
@@ -42,7 +77,14 @@ const sumaCompra = () =>{
             let precio = Math.round(data.id / (1 + '0'.repeat(Number(data.id.toString().length - 2))))
             
           totalCompra += precio
-            document.getElementById("total-compra").innerHTML = `${cart.length >= 0 ? `<h3>Total Pedido: U$D  ${totalCompra} </h3>` : "Aun no Agregas un Producto al Carrito 5" } `
+            document.getElementById("total-compra").innerHTML = `${cart.length >= 0 ? 
+                `
+                    <h3>Total Pedido: U$D  ${totalCompra} </h3>
+                    <button onclick="finalizarPedido()" class="btn btn-success "> Pagar  </button>
+                    <button onclick="eliminarPedido()" class="btn btn-danger "> Eliminar </button>
+                ` 
+                :
+                    "Aun no Agregas un Producto al Carrito 5" } `
             console.log(totalCompra);
             
         })

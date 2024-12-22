10c26227b62462edb64372d585f7663e7c5bcace
Francary <francarymolinapaez@gmail.com>
2024-12-22 10:18:57 -0300

    Cart 2.0 Delete Item y Mejorar Codigo de apiCart

--- javascript/apiCart.js
+++ javascript/apiCart.js
@@ -2,11 +2,11 @@ const buscarProductos = async () => {
     try {
         const APIKEY = "6pt3k8glLgKKFcfTjanvTvZaORINjNpC3SEn76dlnIEe0D4YbLCRTXFo"
 
-     // Recuperar el carrito del localStorage
-     let cart = JSON.parse(localStorage.getItem("Pedido")) || [];
+        // Recuperar el carrito del localStorage
+        let cart = JSON.parse(localStorage.getItem("Pedido")) || [];
+        document.getElementById("container-productos").innerHTML = '';
 
-
-         for (const item of cart) {
+        cart.forEach(async (item, index) => {
             const id = item.id 
             const URL = `https://api.pexels.com/v1/photos/${id}`
             const res = await fetch(URL, {
@@ -17,23 +17,22 @@ const buscarProductos = async () => {
             })
 
             const data = await res.json();
-            console.log("Respuesta de Consulta:", data);
-
+                    
             // Funcion de para calcular un precio segun el id del Producto
 
             let precio = Math.round(data.id / (1 + '0'.repeat(Number(data.id.toString().length - 2))))
 
             let cardProducto =
-                `        
+            `    
             <div class="card-productos">
                 <img class="producto" src="${data.src.tiny}" alt="${data.alt}">
                 <p> U$D ${precio}</p>
-                <button onclick="getCart(${data.id})"class="btn btn-primary">Pedir</button>
+                <button onclick="deleteItem(${index})" class="btn btn-danger">Eliminar</button>
             </div>
-        `
-
+            `
+          
             document.getElementById("container-productos").insertAdjacentHTML("beforeend", cardProducto)
-        }
+        })
     } catch (error) {
 
     }
--- javascript/cart.js
+++ javascript/cart.js
@@ -3,9 +3,16 @@ let cart = []
 const getCart = (id) =>{
     cart.push({id})
     alert(`Se Agrego al carrito ${id}`)
+    localStorage.setItem("Pedido", JSON.stringify(cart));
 }
+
+const deleteItem = (index) => {
+    let cart = JSON.parse(localStorage.getItem("Pedido")) || [];      
+    cart.splice(index, 1);
+    localStorage.setItem("Pedido", JSON.stringify(cart));
+    buscarProductos();
+};
 
 window.addEventListener("beforeunload",()=>{
     localStorage.setItem("Pedido",JSON.stringify(cart))
     });
-    
